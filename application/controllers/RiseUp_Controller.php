5bdc5ddb1e508e60df3c0bf03731e5e925ea4c8b
GbSardinha <goncalosardinha@gmail.com>
2019-11-03 16:18:44 +0100

    3rd Commit : Connection to DB , added proper edits, updates and creates

--- application/controllers/RiseUp_Controller.php
+++ application/controllers/RiseUp_Controller.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
 class RiseUp_Controller extends CI_Controller
 {
     public function __construct()
@@ -8,32 +7,27 @@ class RiseUp_Controller extends CI_Controller
         parent::__construct();
         $this->load->model('RiseUp_model');
     }
-
     public function index()
     {
         $data['result'] = $this->RiseUp_model->getAllData();
         $this->load->view('index_riseup', $data);
     }
-
+    
     public function create()
     {
         $this->RiseUp_model->createData();
         redirect('RiseUp_Controller');
     }
-
-
     public function edit($id)
     {
         $data['userData'] = $this->RiseUp_model->getData($id);
         $this->load->view('riseup_edit', $data);
     }
-
     public function update($id)
     {
         $this->RiseUp_model->updateData($id);
         redirect('RiseUp_Controller');
     }
-
     public function delete($id)
     {
         $this->RiseUp_model->deleteData($id);
--- application/models/RiseUp_model.php
+++ application/models/RiseUp_model.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
 class RiseUp_model extends CI_Model
 {
     public function __construct()
@@ -11,31 +10,31 @@ class RiseUp_model extends CI_Model
     function createData()
     {
         $data = array(
-            'inputName' => $this->input->post('inputName'),
-            'inputUsername' => $this->input->post('inputUsername'),
-            'inputEmail' => $this->input->post('inputEmail')
+            'name' => $this->input->post('name'),
+            'username' => $this->input->post('username'),
+            'email' => $this->input->post('email')
         );
         $this->db->insert('Users', $data);
     }
 
     function getAllData()
     {
-        $query = $this->db->query('SELECT * FROM Users');
+        $query = $this->db->query('SELECT * FROM Users ORDER BY id ASC');
         return $query->result();
     }
 
     function getData($id)
     {
-        $query = $this->db->query('SELECT * FROM Users WHERE `id` =' . $id);
-        return $query->userData();
+        $query = $this->db->query('SELECT * FROM Users WHERE `id` =' .$id);
+        return $query->row();
     }
 
     function updateData($id)
     {
         $data = array(
-            'inputName' => $this->input->post('inputName'),
-            'inputUsername' => $this->input->post('inputUsername'),
-            'inputEmail' => $this->input->post('inputEmail')
+            'name' => $this->input->post('name'),
+            'username' => $this->input->post('username'),
+            'email' => $this->input->post('email')
         );
         $this->db->where('id', $id);
         $this->db->update('Users', $data);
